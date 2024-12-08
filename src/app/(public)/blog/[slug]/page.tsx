0c60d89ad0999f6e3f33e70392fbb52785d06b63
Samir Mirzaliyev <themirzaliyev@gmail.com>
2024-12-08 23:40:27 +0400

    fixed slug issue

--- src/app/(public)/blog/[slug]/page.tsx
+++ src/app/(public)/blog/[slug]/page.tsx
@@ -1,11 +1,11 @@
 import { PostDetails } from "@/modules/blog/interface/ui/post-details/post-details";
 import React from "react";
 
-interface PageProps {
-  params: { slug: string };
-}
-
-export default function Page({ params }: PageProps) {
-  const { slug } = params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
   return <PostDetails slug={slug} />;
 }
