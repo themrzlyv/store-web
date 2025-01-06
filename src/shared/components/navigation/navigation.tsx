c2e69247a8fc89d6a31a4e22492627529135bdaf
Samir Mirzaliyev <themirzaliyev@gmail.com>
2025-01-07 00:39:31 +0400

    add rating button

--- src/shared/components/navigation/navigation.tsx
+++ src/shared/components/navigation/navigation.tsx
@@ -3,10 +3,12 @@ import { usePathname } from "next/navigation";
 import { SwitchTheme } from "../switch-theme/switch-theme";
 import { NavigationItem } from "./navigation-item";
 import { navigationSlots } from "./navigation.slots";
+import { MdStarBorder } from "react-icons/md";
+import Button from "@/ui/button";
 
 export function Navigation() {
   const pathname = usePathname();
-  const { root} = navigationSlots()
+  const { root } = navigationSlots();
   return (
     <nav className={root()}>
       {menuRoutes.map(route => {
@@ -16,6 +18,10 @@ export function Navigation() {
         );
       })}
       <SwitchTheme className="hidden md:flex" />
+      {/* TODO: Add rating logic */}
+      <Button variant="ghost" size="sm">
+        <MdStarBorder size={20} />
+      </Button>
     </nav>
   );
 }
